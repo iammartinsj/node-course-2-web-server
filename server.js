84545730bca8d9c9f483356bf79c6a9890df7d06
Steve Martin <steve.sanjose@redcoresolutions.com>
2018-12-27 22:32:49 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,61 @@
+//Third Party Modules
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+ 
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append to server')
+    } else {
+      next();
+    }
+  });
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs'); 
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    welcomeMessage: 'Welcome User',
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up in port 3000');
+});
